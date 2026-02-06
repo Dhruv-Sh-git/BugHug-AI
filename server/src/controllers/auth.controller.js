3f217d7da51cd7bcf6a47478a2052ac41308a388
Dhruv-Sh-git <dhruvshaswat25@gmail.com>
2026-02-07 03:07:15 +0530

    backend setup and mongoDB env required

---
+++ server/src/controllers/auth.controller.js
@@ -0,0 +1,48 @@
+import User from "../models/User.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+export const signup = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    const exists = await User.findOne({ email });
+    if (exists) return res.status(400).json({ message: "User exists" });
+
+    const hashed = await bcrypt.hash(password, 10);
+
+    await User.create({ name, email, password: hashed });
+    res.status(201).json({ message: "User created" });
+
+  } catch {
+    res.status(500).json({ message: "Signup failed" });
+  }
+};
+
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+    if (!user) return res.status(404).json({ message: "User not found" });
+
+    const match = await bcrypt.compare(password, user.password);
+    if (!match) return res.status(401).json({ message: "Invalid credentials" });
+
+    const token = jwt.sign(
+      { id: user._id },
+      process.env.JWT_SECRET,
+      { expiresIn: "7d" }
+    );
+
+    res.cookie("token", token, {
+      httpOnly: true,
+      sameSite: "lax"
+    });
+
+    res.json({ message: "Login successful" });
+
+  } catch {
+    res.status(500).json({ message: "Login failed" });
+  }
+};
