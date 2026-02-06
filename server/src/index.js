a27d1677f6028084352aea81505189813e2ba0db
Dhruv-Sh-git <dhruvshaswat25@gmail.com>
2026-02-06 23:19:20 +0530

    set up base file and database schema

---
+++ server/src/index.js
@@ -0,0 +1,28 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+import authRoutes from "./routes/auth.routes.js";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors({
+  origin: "http://localhost:3000",
+  credentials: true
+}));
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB connected");
+    app.listen(5000, () => console.log("Server running on port 5000"));
+  })
+  .catch(err => console.log(err));
